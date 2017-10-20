d6d0c622d0dab5a1df19849164005ef339b270db
samuelmormin <samuel.mormin@hetic.net>
2017-10-20 15:34:57 +0200

    :art: Functions.php updated

--- wp-content/themes/basquiat/functions.php
+++ wp-content/themes/basquiat/functions.php
@@ -1,7 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kylehazan
- * Date: 11/10/2017
- * Time: 14:26
- */
+    define( 'THEME_PATH' ,          get_template_directory()            );
+    //define( 'TEMPLATE_PATH' ,       THEME_PATH .    '/templates'        );
+    define( 'THEME_URL' ,           get_template_directory_uri()        );
+    define( 'CSS_URL' ,             THEME_URL .    '/assets/styles'       );
+    define( 'IMAGES_URL' ,          THEME_URL .    '/assets/images'       );
+    define( 'JS_URL' ,              THEME_URL .    '/assets/scripts'      );
+    //define( 'FAVICONS_URL' ,        THEME_URL .    '/dist/favicon'      );
+    //define( 'ADMIN_IMAGES_URL' ,    IMAGES_URL .   '/admin'             );
+    foreach ( glob( THEME_PATH . "/inc/*.php" ) as $file ) {
+        include_once $file;
+    }
+?>
