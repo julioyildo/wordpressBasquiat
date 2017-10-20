1c0d9ca82e37962a521251ef363eb41475376c94
votrepseudojulioyildogit config --global user.email votre@emailserhat.yildirim@hetic.net <serhat.yildirim@hetic.net>
2017-10-20 13:37:37 +0200

    test footer

--- wp-content/themes/basquiat/footer.php
+++ wp-content/themes/basquiat/footer.php
@@ -1,7 +1,14 @@
 <footer id="footer">
     <!--
         Tout le contenu de la partie head de mon site
+        
      -->
+
+    <div style ='color: red;'>
+    Je suis le super footer
+    
+    </div>
+
 </footer>
 
 <!-- Execution de la fonction wp_footer() obligatoire ! -->
