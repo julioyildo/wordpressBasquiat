184a75fc5baf1a18907c5dbc96e40a60e58a323a
samuelmormin <samuel.mormin@hetic.net>
2017-11-27 18:58:47 +0100

    :bug: Fixing menus names conflicts

--- wp-content/themes/basquiat/inc/menus.php
+++ wp-content/themes/basquiat/inc/menus.php
@@ -4,10 +4,7 @@ add_action( 'after_setup_theme', 'menus_du_themes' );
 
 function menus_du_themes() {
   register_nav_menu( 'header', 'Menu entête' );
-<<<<<<< Updated upstream
   register_nav_menu( 'footer', 'Menu footer' );
-=======
   register_nav_menu( 'footer', 'Menu bas de page' );
   register_nav_menu( 'galleries', 'Menu galeries' );
->>>>>>> Stashed changes
 }
